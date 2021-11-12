bacddb18058a59279b1149d964fb28033f84fa36
Alexander Diva <alexanderdivagraelbangun@gmail.com>
2021-11-12 22:32:30 +0700

    meet 6

---
+++ Intermediate1/Meeting6/practice.js
@@ -0,0 +1,20 @@
+// function counter(){
+//     for(var count = 0; ; count++){
+//         console.log(count);
+//         if(count === 10){
+//             return;
+//         }
+//     }
+    
+// }
+// counter();
+
+// ------------------------------------------------
+
+function magic(){
+    return function calc(x){
+        return x * 2;
+    }
+}
+var answer = magic();
+console.log(answer(1337));
