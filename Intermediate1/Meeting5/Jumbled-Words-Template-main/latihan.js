f0f1b87a0dd90b66dfda786e0481c79b77dccf3a
Alexander Diva <alexanderdivagraelbangun@gmail.com>
2021-11-26 18:43:54 +0700

    meeting 5 finish

---
+++ Intermediate1/Meeting5/Jumbled-Words-Template-main/latihan.js
@@ -0,0 +1,15 @@
+function shuffleWord(word) {
+    var shuffleWord = '';
+    word = word.split('');
+
+    while (word.length > 0){
+        var number = word.length * Math.random();
+        console.log(number);
+        // console.log(word.length * Math.random() << 0, 1)
+        shuffleWord += word.splice(word.length * Math.random(), 1);
+    }
+
+    return shuffleWord;
+}
+
+console.log(shuffleWord('TEST'));
