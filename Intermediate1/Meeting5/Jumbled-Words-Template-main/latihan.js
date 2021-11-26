139e554d231138284ed0c16547274832d7535b61
Alexander Diva <alexanderdivagraelbangun@gmail.com>
2021-11-26 19:42:31 +0700

    Meeting 5

--- Intermediate1/Meeting5/Jumbled-Words-Template-main/latihan.js
+++ Intermediate1/Meeting5/Jumbled-Words-Template-main/latihan.js
@@ -1,15 +1,21 @@
-function shuffleWord(word) {
-    var shuffleWord = '';
-    word = word.split('');
+// function shuffleWord(word) {
+//     var shuffleWord = '';
+//     word = word.split('');
 
-    while (word.length > 0){
-        var number = word.length * Math.random();
-        console.log(number);
-        // console.log(word.length * Math.random() << 0, 1)
-        shuffleWord += word.splice(word.length * Math.random(), 1);
-    }
+//     while (word.length > 0){
+//         var number = word.length * Math.random();
+//         console.log(number);
+//         // console.log(word.length * Math.random() << 0, 1)
+//         shuffleWord += word.splice(word.length * Math.random(), 1);
+//     }
 
-    return shuffleWord;
+//     return shuffleWord;
+// }
+
+// console.log(shuffleWord('TEST'));
+
+
+
+for (let i = 0; i < 10; i=i2) {
+    console.log(i);
 }
-
-console.log(shuffleWord('TEST'));
