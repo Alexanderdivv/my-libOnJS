b56dbaad748d1dd967e21e551feb487d86148703
Alexander Diva <alexanderdivagraelbangun@gmail.com>
2021-12-04 01:24:06 +0700

    meeting 9

---
+++ Intermediate1/Meeting9/latihan.js
@@ -0,0 +1,9 @@
+// Variabel let dan var
+var usia = 10;
+var usia = 20;
+console.log(usia);
+// jika var diganti dengan let, maka akan error
+// karena let tidak membiarkan redeclaration
+
+// let 1myname = "John";
+// console.log(1myname);
