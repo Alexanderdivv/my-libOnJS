10b32c97f02a50aaa46b4230aa78fdf727b919c4
Alexander Diva <alexanderdivagraelbangun@gmail.com>
2021-12-04 14:23:37 +0700

    meetong 9 penjelasan

--- Intermediate1/Meeting9/latihan.js
+++ Intermediate1/Meeting9/latihan.js
@@ -1,9 +1,41 @@
 // Variabel let dan var
-var usia = 10;
-var usia = 20;
-console.log(usia);
-// jika var diganti dengan let, maka akan error
-// karena let tidak membiarkan redeclaration
+const myName;
+myName= "Ayu";
 
-// let 1myname = "John";
-// console.log(1myname);
+console.log(myName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // jika var diganti dengan let, maka akan error
+// // karena let tidak membiarkan redeclaration
+
+// // let 1myname = "John";
+// // console.log(1myname);
+
+
