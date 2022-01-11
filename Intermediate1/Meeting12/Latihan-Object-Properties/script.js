5fb3c55b81af3af75e1f02c23acc2ed9a3ab732a
Alexander Diva <alexanderdivagraelbangun@gmail.com>
2022-01-11 09:54:10 +0700

    latihan object

---
+++ Intermediate1/Meeting12/Latihan-Object-Properties/script.js
@@ -0,0 +1,19 @@
+//isi titik-titik dibawah ini ya
+//buat object dengan nama ruang
+var ... = {
+  tipe: "Balok",
+  ...: "...", //deskripsikan properties warnanya
+  sisi: ..., //coba deskripsikan banyak sisinya
+  panjang: 10,
+  lebar: 5,
+  tinggi: 4
+};
+//coba akses nilai dari panjang, lebar dan tinggi
+//buat agar console menampilkan volume balok
+console.log(...............................);
+//buat agar console menampilkan volume balok
+//sekarang gunakan function untuk menampilkan volumenya
+function volume(){
+  return "Volume balok adalah: " + ...........;
+}
+console.log(volume());
