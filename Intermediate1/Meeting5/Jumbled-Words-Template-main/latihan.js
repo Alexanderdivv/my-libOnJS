6249f0e75de631b1d5d12a9a18b3d2d43b5d97b6
Alexander Diva <alexanderdivagraelbangun@gmail.com>
2022-01-26 20:12:55 +0700

    Explanation splice, split, math random

--- Intermediate1/Meeting5/Jumbled-Words-Template-main/latihan.js
+++ Intermediate1/Meeting5/Jumbled-Words-Template-main/latihan.js
@@ -1,21 +1,32 @@
-// function shuffleWord(word) {
-//     var shuffleWord = '';
-//     word = word.split('');
+function shuffleWord(word) {
+    var shuffleWord = '';
+    word = word.split('');
 
-//     while (word.length > 0){
-//         var number = word.length * Math.random();
-//         console.log(number);
-//         // console.log(word.length * Math.random() << 0, 1)
-//         shuffleWord += word.splice(word.length * Math.random(), 1);
-//     }
+    while (word.length > 0){
+        var number = word.length * Math.random();
+        console.log(number);
+        // console.log(word.length * Math.random() << 0, 1)
+        shuffleWord += word.splice(word.length * Math.random(), 1);
+    }
+        console.log(typeof(shuffleWord));
+    return shuffleWord;
+}
 
-//     return shuffleWord;
+console.log(shuffleWord('TEST'));
+
+
+
+// for (let i = 0; i < 10; i=i2) {
+//     console.log(i);
 // }
 
-// console.log(shuffleWord('TEST'));
+// var angka = 10 * Math.random()
+// console.log(parseInt(angka << 0));
+
+// console.log(angka >> 1);
 
 
 
-for (let i = 0; i < 10; i=i2) {
-    console.log(i);
-}
+// split wordd/string
+// let words = 'JAVA'
+// console.log(words.split(''));
