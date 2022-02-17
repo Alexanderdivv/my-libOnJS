7d952e8f76016a004d5134c602960e48dd01eb24
Alexander Diva <alexanderdivagraelbangun@gmail.com>
2022-02-17 20:11:34 +0700

    Meeting10

--- Intermediate1/Meeting10/latihan.js
+++ Intermediate1/Meeting10/latihan.js
@@ -1,5 +1,30 @@
-// string expression
-score = 70.3;
-console.log("Tipe Score, " + typeof(score));
-stringScore = String(score);
-console.log("Tipe StringScore, "+ typeof(stringScore));
+// // string expression
+// score = 70.3;
+// console.log("Tipe Score, " + typeof(score));
+// stringScore = String(score);
+// console.log("Tipe StringScore, "+ typeof(stringScore));
+
+// ---pemisah---
+
+// memeriksa tipe data
+// score = 100;
+// console.log("Tipe Score, " + typeof score);
+// score = "100";
+// console.log("Tipe Score, " + typeof score);
+// score = parseFloat(score);
+// console.log("Tipe Score, " + typeof score);
+// score = String(score);
+// console.log("Tipe Score, " + typeof score);
+
+// ---pemisah---
+
+// exponents
+// console.log("Score is, " + 2 ** 10);
+
+var score = 39;
+console.log(typeof score);
+
+score = String(score);
+console.log(typeof score);
+a++;
+a = a + 1;
