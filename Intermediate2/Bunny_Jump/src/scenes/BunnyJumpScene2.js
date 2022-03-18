1137c397e24213c29c34ac203dcc524a3a7ce7d6
Alexander Diva <alexanderdivagraelbangun@gmail.com>
2022-03-18 15:50:47 +0700

    Ghost buster

--- Intermediate2/Bunny_Jump/src/scenes/BunnyJumpScene2.js
+++ Intermediate2/Bunny_Jump/src/scenes/BunnyJumpScene2.js
@@ -88,7 +88,7 @@ export default class BunnyJumpScene extends Phaser.Scene {
       const platformChild = child;
       const scrollY = this.cameras.main.scrollY;
       // @ts-ignore
-      if (platformChild.y >= scrollY + 700) {
+      if (platformChild.y >= scrollY + 600) {
         // @ts-ignore
         platformChild.y = scrollY - Phaser.Math.Between(0, 200);
         // @ts-ignore
