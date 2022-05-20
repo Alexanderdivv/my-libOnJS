9fd6794c5a46fa6e7b295e18961227d89e348846
Alexander Diva <alexanderdivagraelbangun@gmail.com>
2022-05-20 23:10:55 +0700

    Make game window fit to browser

--- AleBros/src/main.js
+++ AleBros/src/main.js
@@ -1,18 +1,26 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
-import HelloWorldScene from './scenes/HelloWorldScene'
+import HelloWorldScene from "./scenes/HelloWorldScene";
 
 const config = {
-	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
-	},
-	scene: [HelloWorldScene]
-}
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
 
-export default new Phaser.Game(config)
+  scale: {
+    // fit the game in the window
+    // mode: Phaser.Scale.FIT,
+    mode: Phaser.Scale.RESIZE,
+    // autoCenter: Phaser.Scale.CENTER_BOTH,
+    autoCenter: Phaser.Scale.NO_CENTER,
+  },
+  scene: [HelloWorldScene],
+};
+
+export default new Phaser.Game(config);
