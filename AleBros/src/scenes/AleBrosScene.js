3c4be2950d14d80849e996d1aa9d639f7e59e794
Alexander Diva <alexanderdivagraelbangun@gmail.com>
2022-05-21 22:08:49 +0700

    update character flipping

--- AleBros/src/scenes/AleBrosScene.js
+++ AleBros/src/scenes/AleBrosScene.js
@@ -4,15 +4,71 @@ export default class AleBrosScene extends Phaser.Scene {
     super("ale-bros-scene");
   }
 
-  init() {}
+  init() {
+    this.platform = undefined;
+    this.player = undefined;
+    this.cursors = undefined;
+  }
 
   preload() {
     this.load.image("background", "images/sky.png");
+    this.load.image("platform", "images/platform.png");
+    this.load.spritesheet("dude", "images/player_tilesheet.png", {
+      frameWidth: 80,
+      frameHeight: 110,
+    });
   }
 
   create() {
+    this.cursors = this.input.keyboard.createCursorKeys();
     this.add.image(400, 300, "background").setScale(2);
+    this.platform = this.physics.add.staticGroup();
+    this.platform.create(600, 550, "platform").setScale(2).refreshBody();
+    this.player = this.createAnimation();
+
+    this.physics.add.collider(this.player, this.platform);
   }
 
-  update() {}
+  update() {
+    // if player walks to the left
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+      this.player.flipX = true;
+      this.player.anims.play("left", true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+      this.player.flipX = false;
+      this.player.anims.play("right", true);
+    } else {
+      this.player.setVelocityX(0);
+      this.player.anims.play("turn");
+    }
+  }
+
+  createAnimation() {
+    const player = this.physics.add.sprite(600, 450, "dude");
+    player.setCollideWorldBounds(true);
+    player.setBounce(0.2);
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 9, end: 10 }),
+      frameRate: 10,
+      repeat: -1, //mengulangu animasi (loop)
+      // mirror
+    });
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "dude", frame: 0 }],
+      frameRate: 20,
+    });
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", { start: 9, end: 10 }),
+      frameRate: 10,
+      repeat: -1, //mengulangu animasi (loop)
+    });
+    player.anims.play("turn");
+
+    return player;
+  }
 }
